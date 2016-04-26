1be5d0542d7589e5fa0e6c8f97ce0f6619d96211
oreligr <oreligr@rambler.ru>
2016-04-26 16:29:31 +0200

    dasdasads

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	BeanFactory factory= new XmlBeanFactory(new FileSystemResource("spring.xml"));
+	System.out.rpintnl("asdas");
     }
 }
